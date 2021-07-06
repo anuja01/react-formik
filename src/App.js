9bfd1b4e8a9a28bea1363fc9d5bdf493526dd0f0
Anuja Ulpathakumbura <anuja.ulp@gmail.com>
2021-07-06 23:38:36 +0800

    form with Formik components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FormikForm from './components/formik-component-form';
 import Basic from "./components/usual-form";
 
 function App() {
   return (
     <div>
       <Basic />
+      <FormikForm />
     </div>
   );
 }
